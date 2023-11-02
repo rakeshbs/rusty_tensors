69334192d10ad7161807de499589028318a524c1
rakeshbs <rakeshbs@gmail.com>
2023-11-02 17:49:47 +0530

    Added basic computational graph test

--- examples/basics.rs
+++ examples/basics.rs
@@ -1,25 +1,26 @@
 extern crate rusty_tensors;
+use ndarray::array;
 use rusty_tensors::tensor::*;
 
 fn main() {
-    let a = tensor(&[1.0, 2.0, 3.0], true);
-    let b = tensor(&[4.0, 5.0, 6.0], true);
+    let a = tensor(array![[1.0], [2.0], [3.0]], true);
+    let b = tensor(array![[1.0], [2.0], [3.0]], true);
 
-    let num_epochs = 10;
+    let num_epochs = 1;
 
     for i in 0..num_epochs {
-        let c = add(&a, &b);
-        let d = mul(&a, &b);
-        let e = add(&c, &d);
+        println!("Epoch {}", i);
+        print!("");
+        let c = add(&pow(&a, 2.), &pow(&b, 2.));
+        let d = sum(&c);
 
-        println!("e: {:?}", e.output());
-        e.backward();
-        a.step(0.1);
-        b.step(0.1);
+        println!("d: {:?}\n", d.output());
+        d.backward();
+        a.step(0.01);
+        b.step(0.01);
         a.zero_grad();
         b.zero_grad();
+        println!("a: {:?}\n", a.output());
+        println!("b: {:?}\n", b.output());
     }
-
-    println!("a: {:?}", a.output());
-    println!("b: {:?}", b.output());
 }
